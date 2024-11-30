755368f7996742a4ed6d308dc8a6adb358460ea9
tech-design-pilot <tech.design.pilot@gmail.com>
2024-11-30 08:27:03 -0500

    Initial commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,7 +9,14 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync(),
-  provideFirebaseApp(() => initializeApp({ "projectId": "angular-todo-app-27115", "appId": "1:135850969668:web:af10f22d6a3893acf08a11", "storageBucket": "angular-todo-app-27115.firebasestorage.app", "apiKey": "AIzaSyCEmhtFhlnlvSYgOra8UOTc_i1lxZOA67E", "authDomain": "angular-todo-app-27115.firebaseapp.com", "messagingSenderId": "135850969668" })),
+  provideFirebaseApp(() => initializeApp({
+    "projectId": "your-project-id",
+    "appId": "your-app-id",
+    "storageBucket": "your-storage-bucket",
+    "apiKey": "your-api-key",
+    "authDomain": "your-api-key",
+    "messagingSenderId": "your-messaging-sender-id"
+  })),
   provideFirestore(() => getFirestore()),
   provideNativeDateAdapter()]
 };
